defe808533f6789f5844af5998de13e8ebb18740
Dima <dimabuhaidima@gmail.com>
2023-05-23 05:14:31 +0300

    add coments

--- labsevennine/users/views.py
+++ labsevennine/users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import redirect, render
-from django.views import View
+from django.views import View #Підключає базовий клас View для створення класу перегляду.
 from django.contrib.auth.decorators import login_required
-from .forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from .forms import UserCreationForm #Підключає форму UserCreationForm
+from django.contrib.auth import authenticate, login #Підключає функції для аутентифікації та входу користувача.
 from django.contrib.auth.mixins import LoginRequiredMixin #use to ensure that only authenticated users can access a view
 from django.urls import reverse_lazy #provides a form for updating an existing object
 from django.views.generic.edit import UpdateView
@@ -18,13 +18,21 @@ def user_home(request):
     return render(request, "user_home.html", context)
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
+    '''
+    Клас-перегляд, який дозволяє користувачеві оновлювати свій профіль. Використовує клас
+    LoginRequiredMixin для перевірки автентифікації користувача. Визначає модель User, поля,
+    які можна редагувати, шаблон "user_update_form.html"
+    '''
     model = User
     fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
     template_name = 'user_update_form.html'
     success_url = reverse_lazy('user_home')
 
 class Register(View):
-
+    '''
+    Клас-перегляд, який відповідає за реєстрацію нового користувача.
+    Визначає методи get і post для отримання та обробки форми реєстрації.
+    '''
     template_name = 'registration/register.html'
 
     def get(self, request): 
